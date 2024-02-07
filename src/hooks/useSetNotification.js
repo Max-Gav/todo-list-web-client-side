c5cfc3400ab12714ef22579c16273cc699dd1e5f
Maxim Gavrilove <maximgav123@gmail.com>
2024-02-07 15:53:33 +0200

    Fix Worker Unnecessary Redefinition

--- src/hooks/useSetNotification.js
+++ src/hooks/useSetNotification.js
@@ -1,35 +1,45 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import apiRequest from "../utils/apiRequest";
 import WebWorker from "../web workers/WebWorker";
 import alertWorker from "../web workers/alert.worker";
 
-const useSetNotification = (openOverlay,handleClose)=>{
-    const [notificationTime, setNotificationTime] = useState(0);
-    const [inputError, setInputError] = useState(null);
-    const worker = new WebWorker(alertWorker);
-  
-    worker.addEventListener("message", async (event) => {
+const useSetNotification = (openOverlay, handleClose) => {
+  const [notificationTime, setNotificationTime] = useState(0);
+  const [inputError, setInputError] = useState(null);
+  const worker = useRef(null);
+
+  useEffect(() => {
+    worker.current = new WebWorker(alertWorker);
+
+    worker.current.addEventListener("message", async (event) => {
       const taskId = event.data.taskId;
-      const result = await apiRequest("post","/tasks/notification-eligible",{ taskId });
-      if(result.answer){
+      const result = await apiRequest(
+        "post",
+        "/tasks/notification-eligible",
+        {
+          taskId,
+        }
+      );
+      if (result.answer) {
         openOverlay("notification", {
           taskId,
           message: event.data.message,
         });
       }
-    })
-  
-    const createNotification = (props) => {
-      if (notificationTime > 0) {
-        setInputError(null);
-        worker.postMessage({ ...props, notificationTime });
-        handleClose();
-      } else {
-        setInputError("Enter a time in the future!");
-      }
-    };
+    });
+  }, []);
 
-    return {setNotificationTime, inputError, createNotification}
-}
+  const createNotification = (props) => {
+    if (notificationTime > 0) {
+      setInputError(null);
+      worker.current.postMessage({ ...props, notificationTime:1000 });
+      handleClose();
+    } else {
+      setInputError("Enter a time in the future!");
+    }
+  };
 
-export default useSetNotification
+  return { setNotificationTime, inputError, createNotification };
+};
+
+export default useSetNotification;
